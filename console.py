c2d0561e820a2863c15dbe0afbda992ebed055fe
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-10-15 08:38:26 -0700

    The console

--- console.py
+++ console.py
@@ -1,3 +1,24 @@
 #!/usr/bin/python3
 
 """ HBnB console"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    prompt = '(hbnb) '
+
+    def do_quit(self, arg):
+        """Quit the program"""
+        return True
+
+    def do_EOF(self, arg):
+        """Signals end of command line"""
+        return True
+
+    def emptyline(self):
+        """Nothing done when entering empty command"""
+        pass
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
