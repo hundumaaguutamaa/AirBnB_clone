4d0326c5c6cac970fab31d163d7669e16b6d8a28
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-10-15 20:12:06 +0300

    Updating console.py

--- console.py
+++ console.py
@@ -1,15 +1,33 @@
 #!/usr/bin/python3
 
-""" HBnB console"""
-
 import cmd
 import json
 import sys
+import models
 
+from models.base_model import BaseModel
+from models import storage
+from models.amenity import Amenity
+from models.city import City
+from models.state import State
+from models.user import User
+from models.place import Place
+from models.review import Review
+
+""" HBnB console"""
 
 class HBNBCommand(cmd.Cmd):
-    prompt = '(hbnb) '
-    file = None
+    prompt = "(hbnb) "
+    models = {
+    "Amenity",
+    "BaseModel",
+    "City",
+    "Place",
+    "User",
+    "Review"
+    "State",
+
+    }
 
     def do_quit(self, arg):
         """Quit the program"""
@@ -54,6 +72,90 @@ class HBNBCommand(cmd.Cmd):
                 print(instance)
             else:
                 print("Instance Found!")
+    def do_destroy(self, arg):
+        """Delete based on a class name of a given id."""
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+        elif args[0] not in HBNBCommand.__models:
+            print("** class doesn't exist **")
+        elif len(args) == 1:
+            print("** instance id missing **")
+        else:
+            objects_dict = models.storage.all()
+            instance_key = "{}.{}".format(args[0], args[1])
+            instance = objects_dict.get(instance_key)
+            if instance:
+                del objects_dict[instance_key]
+                models.storage.save()
+            else:
+                print("** no instance found **")
+
+    def do_all(self, arg):
+        """
+        Prints all string representations of all instances
+        based on the class name.
+        """
+        objects_dict = models.storage.all()
+        instance_list = []
+
+        if not arg:
+            for key in objects_dict:
+                instance_list.append(str(objects_dict[key]))
+        else:
+            class_names = arg.strip()
+            if class_names in self.__models:
+                for key, value in objects_dict.items():
+                    if value.__class__.__name__ == class_names:
+                        instance_list.append(str(value))
+            else:
+                print("** class doesn't exist **")
+                return
+
+        print(instance_list)
+
+    def do_update(self, arg):
+        """Update an instance based on the class name and id.
+            And saves in to JSON file.      
+        """
+        args = arg.split()
+
+        if len(args) == 0:
+            print("** class name missing **")
+            return
+
+        class_names = args[0]
+        if class_names not in self.__models:
+            print("** class doesn't exist **")
+            return
+
+        if len(args) <= 1:
+            print("** instance id missing **")
+            return
+
+        instance_id = args[1]
+        obj_key = "{}.{}".format(class_name, instance_id)
+        obj_dict = storage.all()
+
+        if obj_key not in obj_dict:
+            print("** no instance found **")
+            return
+
+        instance = obj_dict[obj_key]
+
+        if len(args) <= 2:
+            print("** attribute name missing **")
+            return
+
+        attrib_name = args[2]
+        if len(args) <= 3:
+            print("** value missing **")
+            return
+
+        attrib_value = args[3]
+        setattr(instance, attrib_name, attrib_value)
+        instance.save()
     
 
 
