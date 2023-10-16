09171391dbc7a3e81072183acb1ccdb39d8cf89e
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-10-15 18:20:50 -0700

    Class review

--- models/review.py
+++ models/review.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""Defines the Review class."""
+
+from models.base_model import BaseModel
+
+
+class Review(BaseModel):
+    """ Represent the review class."""
+
+    place_id = ""
+    user_id = ""
+    text = ""
