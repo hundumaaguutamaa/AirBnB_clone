d7302c8a17eeb7df6e906cd5e05448a91bcccfa5
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-10-15 18:14:44 -0700

    Class city

--- models/city.py
+++ models/city.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+"""Defines the City class."""
+
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """ Represent a state id, name of the city."""
+
+    state_id = ""
+    name = ""
