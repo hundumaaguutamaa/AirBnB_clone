b57bdbbf619d73833a87fcfcf3913da796b65103
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-10-15 18:22:53 -0700

    Class state

--- models/state.py
+++ models/state.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Defines the State class."""
+
+from models.base_model import BaseModel
+
+
+class State(BaseModel):
+    """Represent the state class attribute. """
+
+    name = ""
