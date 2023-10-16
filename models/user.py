6d44a0f68b74dc87c562f4fcea67f5fd175e796b
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-10-15 18:24:34 -0700

    Class user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""Defines the User class."""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Represent a User class attributes."""
+
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
