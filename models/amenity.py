9ca0df9a8bc48bf4eab43b819d02c97abb59e6d2
Hunduma Tesfaye <hundumaa.guutamaa@gmail.com>
2023-10-15 17:51:55 -0700

    Amenity class

--- models/amenity.py
+++ models/amenity.py
@@ -1,1 +1,10 @@
 #!/usr/bin/python3
+"""Defines the Amenity class."""
+
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Amenity class inherited from BaseModel"""
+
+    name = ""
