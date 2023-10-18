43fd7df3beb602b005482b0fd0f3e2f11bc081cf
Hunduma Tesfaye Gutema <hundumaa.guutamaa@gmail.com>
2023-10-18 05:45:56 +0300

    Updatin base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -4,9 +4,12 @@
 
 import uuid
 from datetime import datetime
+import models
 
 class BaseModel:
+    """Project super class."""
     def __init__(self, *args, **kwargs):
+        """Initializing base_model."""
         if kwargs:
             for key, value in kwargs.items():
                 if key == "created_at" or key == "updated_at":
@@ -19,12 +22,16 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """Formating the printing behavior of base_model class."""
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
+        """Updating current time."""
         self.updated_at = datetime.now()
+        models.storage.save()
 
     def to_dict(self):
+        """Returning key and value to dict_copy."""
         dict_copy = self.__dict__.copy()
         dict_copy["__class__"] = self.__class__.__name__
         dict_copy["created_at"] = dict_copy["created_at"].isoformat()
